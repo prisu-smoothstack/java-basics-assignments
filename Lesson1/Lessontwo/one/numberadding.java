fd4777284a6b02e01120a95bfbdd3239d2e1de9e
Emily Stringer <emily.stringer@smoothstack.com>
2021-07-20 19:09:33 -0400

    Fixy

---
+++ Lesson1/Lessontwo/one/numberadding.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.ss.assignment.one;
+
+/**
+ * @author Space Prius
+ *
+ */
+public class numberadding {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int prev;
+		int total = 0;
+		int errors = 0;
+		for (String str : args) {
+			try {
+				prev = Integer.parseInt(str);
+			}
+			catch (NullPointerException NPE) {
+				prev = 0;
+				errors++;
+			}
+			catch (NumberFormatException NFE) {
+				prev = 0;
+				errors++;
+			}
+			total += prev;
+		}
+		System.out.println("Your Sum:" + String.valueOf(total));
+		if (errors > 0) {
+			System.out.println("Don't think I didn't catch you trying any of that funny stuff, I caught " + String.valueOf(errors) + " Exceptions!");
+		}
+	}
+
+}
---
+++ Lesson1/Lessontwo/two/arraychecking.java
@@ -0,0 +1,37 @@
+package com.ss.assignment.two;
+import java.util.PriorityQueue;
+public class arraychecking {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) { 
+		//Array, threw one letter in there for exception handling
+		int[][] map = { {5, 6}, {4, 12}, {25, 14}, {6, 3} };
+	    int max = 0;
+	    int pos1 = -1;
+	    int pos2 = -1;
+	    int[] maxpos = {0,0};
+	    /*
+	     * I decided against using a PriorityQueue for this because I thought it would go against the spirit of the assignment to use one.
+	     * It would go against the spirit of the assignment in my opinion, since a priority queue automatically
+	     * Sorts based off the highest value.
+	     */
+		for (int[] set : map) {
+			//Checking if a number in a set is better than max
+			pos1++;
+			pos2 = -1;
+			for (int n : set) {
+				pos2++;
+				if (n > max) {
+					max = n;
+					maxpos[0] = pos1;
+					maxpos[1] = pos2;
+				}
+			}
+
+		}
+		System.out.println("Highest number: " + max);
+		System.out.println("Located at position: [" + maxpos[0] + "] [" + maxpos[1] + "]");
+	}
+}
